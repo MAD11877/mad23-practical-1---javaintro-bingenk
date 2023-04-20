671310026f2d7f9158ab28eb3c44efa1cfeec08b
Bing En <115916417+bingenk@users.noreply.github.com>
2023-04-20 11:48:45 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,20 +4,17 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
     Scanner in = new Scanner(System.in);
+    
+    //System.out.print("Enter an integer: ");
+    int n = in.nextInt();
+    
+    for (int i = n; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
     
   }
 }
